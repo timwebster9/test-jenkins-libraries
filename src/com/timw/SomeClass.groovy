ff77509bd287821269aca00d63397a2dc74c7545
timwebster9 <tim.webster@gmail.com>
2018-06-06 11:25:29 +0100

    add constructor

--- src/com/timw/SomeClass.groovy
+++ src/com/timw/SomeClass.groovy
@@ -1,11 +1,18 @@
 package com.timw
 
+
 class SomeClass {
 
     public static final String PERF_TEST_REPORT_DIR = 'build/reports/gatling'
 
+    def steps
+
+    public SomeClass(steps) {
+        this.steps = steps
+    }
+
     def performanceTest() {
-        withDocker('hmcts/moj-gatling-image', '-v $WORKSPACE/src/gatling/conf:/etc/gatling/conf') {
+        this.steps.withDocker('hmcts/moj-gatling-image', '-v $WORKSPACE/src/gatling/conf:/etc/gatling/conf') {
             sh 'gatling.sh -sf src/gatling/simulations -df src/gatling/data -bdf src/gatling/bodies -rf $PERF_TEST_REPORT_DIR -m'
         }
     }
