58891be9a7c4929390bab60ee3ee775e0aa0be6a
timwebster9 <tim.webster@gmail.com>
2018-06-12 12:51:04 +0100

    fix class

--- src/com/timw/DocumentPublisher.groovy
+++ src/com/timw/DocumentPublisher.groovy
@@ -8,32 +8,63 @@ import groovy.json.JsonSlurper
 
 class DocumentPublisher {
 
-    def documentClient
+    def steps
+    def product
+    def component
+    def environment
+    def env
     def jsonSlurper = new JsonSlurper()
 
-    DocumentPublisher(String dbUrl, String dbKey) {
-        this.documentClient = new DocumentClient(dbUrl, dbKey, null, null)
+    DocumentPublisher(steps, product, component, environment) {
+        this.steps = steps
+        this.product = product
+        this.component = component
+        this.environment = environment
+        this.env = steps.env
     }
 
-    def publish(String collectionLink, Object data) {
+    void publish(DocumentClient documentClient, String collectionLink, Object data) {
         Document documentDefinition = new Document(data)
-        this.documentClient.createDocument(collectionLink, documentDefinition, null, false)
+        documentClient.createDocument(collectionLink, documentDefinition, null, false)
     }
 
-    def publishAll(String collectionLink, String basedir, String pattern) {
-        def files = this.findFiles(basedir, pattern)
+    void publishAll(DocumentClient documentClient, String collectionLink, String basedir, String pattern) {
+        List files = findFiles(basedir, pattern)
 
-        files.each { -> jsonFilePath
-            def jsonObject = toJson(jsonFilePath)
-            publish(collectionLink, jsonObject)
+        files.each {
+            def jsonObject = wrapWithBuildInfo(new File(it))
+            this.publish(documentClient, collectionLink, jsonObject)
         }
     }
 
-    def toJson(String filePath) {
-        this.jsonSlurper.parse(new java.io.File(filePath))
+    String wrapWithBuildInfo(File file) {
+        Map buildInfo = getBuildInfo()
+        buildInfo.put(file.getName(), fileToJson(file))
+        JsonOutput.toJson(buildInfo).toString()
     }
 
-    def findFiles(String basedir, String pattern) {
+    Object fileToJson(File filePath) {
+        this.jsonSlurper.parse(filePath)
+    }
+
+    static List findFiles(String basedir, String pattern) {
         new FileNameFinder().getFileNames(basedir, pattern)
     }
+
+    Map getBuildInfo() {
+        [
+                product                      : product,
+                component                    : component,
+                environment                  : environment,
+                branch_name                  : env.BRANCH_NAME,
+                build_number                 : env.BUILD_NUMBER,
+                build_id                     : env.BUILD_ID,
+                build_display_name           : env.BUILD_DISPLAY_NAME,
+                job_name                     : env.JOB_NAME,
+                job_base_name                : env.JOB_BASE_NAME,
+                build_tag                    : env.BUILD_TAG,
+                node_name                    : env.NODE_NAME
+        ]
+    }
+
 }
