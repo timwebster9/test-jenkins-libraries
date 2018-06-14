63901ce528341c36d5e50182cd2567c41d5f8713
timwebster9 <tim.webster@gmail.com>
2018-06-14 10:59:13 +0100

    refactor

--- src/com/timw/Gatling.groovy
+++ src/com/timw/Gatling.groovy
@@ -2,15 +2,15 @@ package com.timw
 
 class Gatling implements Serializable {
 
-  public static final String GATLING_REPORTS_RELATIVE_PATH   = 'build/gatling/reports'
-  public static final String GATLING_REPORTS_DIR             = '$WORKSPACE/' + GATLING_REPORTS_RELATIVE_PATH
-  public static final String GATLING_BINARIES_DIR            = '$WORKSPACE/build/gatling/binaries'
-  public static final String GATLING_SIMS_DIR                = '$WORKSPACE/src/gatling/simulations'
-  public static final String GATLING_DATA_DIR                = '$WORKSPACE/src/gatling/data'
-  public static final String GATLING_BODIES_DIR              = '$WORKSPACE/src/gatling/bodies'
-  public static final String GATLING_CONF_DIR                = '$WORKSPACE/src/gatling/conf'
-  public static final String GATLING_IMAGE                   = 'hmcts/moj-gatling-image:2.3.1-1.0'
-  public static final String COSMOSDB_COLLECTION             = 'dbs/jenkins/colls/performance-metrics'
+  public static final String GATLING_REPORTS_DIR   = 'build/gatling/reports'
+  public static final String GATLING_BINARIES_DIR  = 'build/gatling/binaries'
+  public static final String GATLING_SIMS_DIR      = 'src/gatling/simulations'
+  public static final String GATLING_DATA_DIR      = 'src/gatling/data'
+  public static final String GATLING_BODIES_DIR    = 'src/gatling/bodies'
+  public static final String GATLING_CONF_DIR      = 'src/gatling/conf'
+
+  public static final String GATLING_IMAGE         = 'hmcts/moj-gatling-image:2.3.1-1.0'
+  public static final String COSMOSDB_COLLECTION   = 'dbs/jenkins/colls/performance-metrics'
 
   // with Gatling command-line you can't specify a configuration directory, so we need to bind-mount it
   // TODO: use host networking for now as 1) DNS seems to be broken in container and 2) may need it for local perf. testing against localhost
@@ -24,7 +24,11 @@ class Gatling implements Serializable {
 
   def execute() {
     this.steps.withDocker(GATLING_IMAGE, GATLING_RUN_ARGS) {
-      this.steps.sh "gatling.sh -m -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -bf ${GATLING_BINARIES_DIR} -rf ${GATLING_REPORTS_DIR}"
+      this.steps.sh "gatling.sh -m -sf ${WORKSPACE}/${GATLING_SIMS_DIR} \
+                                -df ${WORKSPACE}/${GATLING_DATA_DIR} \
+                                -bdf ${WORKSPACE}/${GATLING_BODIES_DIR} \
+                                -bf ${WORKSPACE}/${GATLING_BINARIES_DIR} \
+                                -rf ${WORKSPACE}/${GATLING_REPORTS_DIR}"
     }
 
     // jenkins gatling plugin
