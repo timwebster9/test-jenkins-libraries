7c234dee546fd25a62f2bdafdadf55fbd453dd8f
timwebster9 <tim.webster@gmail.com>
2018-06-14 11:11:52 +0100

    refactor

--- src/com/timw/Gatling.groovy
+++ src/com/timw/Gatling.groovy
@@ -2,8 +2,8 @@ package com.timw
 
 class Gatling implements Serializable {
 
-  public static final String GATLING_REPORTS_DIR   = '$WORKSPACE/build/gatling/reports'
-  public static final String GATLING_BINARIES_DIR  = '$WORKSPACE/build/gatling/binaries'
+  public static final String GATLING_REPORTS_PATH  = 'build/gatling/reports'
+  public static final String GATLING_REPORTS_DIR   = '$WORKSPACE/' + GATLING_REPORTS_PATH
   public static final String GATLING_SIMS_DIR      = '$WORKSPACE/src/gatling/simulations'
   public static final String GATLING_DATA_DIR      = '$WORKSPACE/src/gatling/data'
   public static final String GATLING_BODIES_DIR    = '$WORKSPACE/src/gatling/bodies'
