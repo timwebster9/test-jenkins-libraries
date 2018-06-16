cfe981faaa01c26ee953ffc5fe1fc84378eda12b
Tim Webster <tim.webster@gmail.com>
2018-06-16 22:23:39 +0100

    refactor

--- vars/kubectlDeploy.groovy
+++ vars/kubectlDeploy.groovy
@@ -1,5 +1,7 @@
-def call(String template) {
-    sh "envsubst < ${template}.tmpl > ${template}.yaml"
-    sh 'kubectl config set-cluster k8s --server=https://kubernetes.default.svc'
-    sh "kubectl apply -f ${template}.yaml"
+def call(String template, String serviceName, String imageName) {
+    withEnv(['IMAGE_NAME=$imageName', 'SERVICE_NAME=serviceName']) {
+        sh "envsubst < ${template}.tmpl > ${template}.yaml"
+        sh 'kubectl config set-cluster k8s --server=https://kubernetes.default.svc'
+        sh "kubectl apply -f ${template}.yaml"
+    }
 }
