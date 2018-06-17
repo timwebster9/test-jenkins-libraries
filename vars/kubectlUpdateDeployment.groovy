119cb62a8b51cce57577444835937a20a51eb7b2
Tim Webster <tim.webster@gmail.com>
2018-06-17 15:37:34 +0100

    configure cluster for update deployment

--- vars/kubectlUpdateDeployment.groovy
+++ vars/kubectlUpdateDeployment.groovy
@@ -1,3 +1,4 @@
 def call(String deploymentName, String imageName) {
+    sh 'kubectl config set-cluster k8s --server=https://kubernetes.default.svc'
     sh "kubectl set image deployment/${deploymentName} ${deploymentName}=${imageName}"
 }
