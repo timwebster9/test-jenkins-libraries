0b679837e9324990240f0d87d131ac7b501f6f86
Tim Webster <tim.webster@gmail.com>
2018-06-17 15:30:07 +0100

    use list for env vars

--- vars/kubectlDeploy.groovy
+++ vars/kubectlDeploy.groovy
@@ -1,4 +1,4 @@
-def call(String template, Map envVars) {
+def call(String template, List envVars) {
     withEnv(envVars) {
         sh "envsubst < ${template}.tmpl > ${template}.yaml"
         sh 'kubectl config set-cluster k8s --server=https://kubernetes.default.svc'
