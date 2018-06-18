f72c47f570f203a12c0bade2372f216e8a3dd73d
timwebster9 <tim.webster@gmail.com>
2018-06-18 19:38:35 +0100

    get branch name for MB

--- vars/setBranchName.groovy
+++ vars/setBranchName.groovy
@@ -1,4 +1,13 @@
 def call() {
-    env.BRANCH_NAME = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+
+    def branchName = "${BRANCH_NAME}"
+
+    if (branchName?.trim()) {
+        env.BRANCH_NAME = branchName
+    }
+    else {
+        env.BRANCH_NAME = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+    }
+
     currentBuild.displayName = "#${BUILD_NUMBER}: ${BRANCH_NAME}"
 }
