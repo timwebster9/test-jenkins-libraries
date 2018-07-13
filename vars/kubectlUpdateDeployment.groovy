7637009cb23b73ff20320f31209625a9ee4eafc4
Tim Webster <tim.webster@gmail.com>
2018-07-13 23:14:48 +0100

    remove login from update

--- vars/kubectlUpdateDeployment.groovy
+++ vars/kubectlUpdateDeployment.groovy
@@ -1,4 +1,3 @@
 def call(String deploymentName, String imageName) {
-    sh 'kubectl config set-cluster k8s --server=https://kubernetes.default.svc'
     sh "kubectl set image deployment/${deploymentName} ${deploymentName}=${imageName}"
 }
