db2e7790aecc2b9a7b700fff3c3e8c5313a1f0df
timwebster9 <tim.webster@gmail.com>
2018-09-13 17:36:37 +0100

    add github api class

---
+++ src/com/timw/GithubAPI.groovy
@@ -0,0 +1,28 @@
+package com.timw
+
+import groovy.json.JsonOutput
+
+class GithubAPI {
+
+    private static final String API_URL = "https://api.github.com/repos/hmcts"
+
+    def steps
+
+    GithubAPI(steps) {
+        this.steps = steps
+    }
+
+    def addLabel(String project, String issueNumber, String... labels) {
+
+        body = JsonOutput.toJson(labels)
+
+        response = this.steps.httpRequest(httpMode: 'POST',
+                                          acceptType: 'APPLICATION_JSON',
+                                          contentType: 'APPLICATION_JSON',
+                                          url: "${API_URL}/${project}/issues/${issueNumber}/labels",
+                                          requestBody: "${body}",
+                                          consoleLogResponseBody: true,
+                                          validResponseCodes: '200')
+    }
+
+}
